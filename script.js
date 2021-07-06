4e74a26b8da562315f3665c173390edefcab3b66
Iana Chyrva <yanachirva@gmail.com>
2021-07-06 16:13:41 +0300

    Second task is done

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ function task1() {
 
 // task1();
 // Uncomment the line above to run function
+
+// Задание_2
+function task2() {
+  const input1 = prompt('Введите первое число');
+
+  if (!isValidNumber(input1)) {
+    console.log('Некорректный ввод!');
+    return;
+  }
+
+  const input2 = prompt('Введите второе число');
+
+  if (!isValidNumber(input2)) {
+    console.log('Некорректный ввод!');
+    return;
+  }
+
+  console.log(`Ответ: ${+input1 + +input2}, ${input1 / input2}.`);
+}
+
+// task2();
+// Uncomment the line above to run function
