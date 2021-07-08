90ff4040cb07c2d256c01cd996df7cca67c208a7
Iana Chyrva <yanachirva@gmail.com>
2021-07-08 10:49:04 +0300

    Fixed style drawbacks

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ function isValidNumber(input) {
   return !!input && !isNaN(input);
 }
 
+const errorMessage = 'Некорректный ввод!';
+
 // Задание_1
 function task1() {
   const input1 = prompt('Введите первое число');
   const input2 = prompt('Введите второе число');
 
-  if (isValidNumber(input1) && isValidNumber(input2)) {
-    if (input2 >= 2 && input2 <= 36) {
-      console.log((+input1).toString(+input2));
-    } else {
-      console.log('Второе число должно быть от 2 до 36 включительно.');
-      return;
-    }
+  if (!isValidNumber(input1) || !isValidNumber(input2)) {
+    console.log(errorMessage);
     return;
   }
 
-  console.log('Некорректный ввод!');
+  if (input2 < 2 || input2 > 36) {
+    console.log('Второе число должно быть от 2 до 36 включительно.');
+    return;
+  }
+
+  console.log(Number(input1).toString(Number(input2)));
 }
 
 // task1();
@@ -28,19 +30,19 @@ function task2() {
   const input1 = prompt('Введите первое число');
 
   if (!isValidNumber(input1)) {
-    console.log('Некорректный ввод!');
+    console.log(errorMessage);
     return;
   }
 
   const input2 = prompt('Введите второе число');
 
   if (!isValidNumber(input2)) {
-    console.log('Некорректный ввод!');
+    console.log(errorMessage);
     return;
   }
 
-  console.log(`Ответ: ${+input1 + +input2}, ${input1 / input2}.`);
+  console.log(`Ответ: ${Number(input1) + Number(input2)}, ${input1 / input2}.`);
 }
 
-// task2();
+task2();
 // Uncomment the line above to run function
