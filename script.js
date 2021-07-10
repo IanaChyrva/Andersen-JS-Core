0963271dc79acfa824f130588e5c560235cc3f71
Iana Chyrva <yanachirva@gmail.com>
2021-07-10 14:00:21 +0300

    First task of lesson-2 is done

--- script.js
+++ script.js
@@ -46,3 +46,30 @@ function task2() {
 
 // task2();
 // Uncomment the line above to run function
+
+// Lesson-2
+// Task 1
+
+const isEqual = (firstObj, secondObj) => {
+  const arr1 = [...Object.keys(firstObj), ...Object.values(firstObj)];
+  const arr2 = [...Object.keys(secondObj), ...Object.values(secondObj)];
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i <= arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const data = { a: 1, b: 1 };
+const data2 = { a: 1, b: 1 };
+const data3 = { a: 1, b: 2 };
+
+console.log(isEqual(data, data2));
+console.log(isEqual(data, data3));
